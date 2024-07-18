96f8277b9620bde119a66a00deeef1411eeaa98b
iulian098 <craciuniulian25@gmail.com>
2024-07-18 22:58:26 +0300

    Minor changes

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,6 +15,12 @@ public class Projectile : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy")) {
+            //Give damage
+        }
         Destroy(gameObject);
         Instantiate(hitVFX, transform.position, Quaternion.identity);
     }
