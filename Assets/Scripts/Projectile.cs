7bab4ad73fbc436e4ca7fea05ef558d8697be1de
iulian098 <craciuniulian25@gmail.com>
2024-07-24 19:24:10 +0300

    CoreGameplay

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -46,5 +46,6 @@ public class Projectile : MonoBehaviour
         }
         Destroy(gameObject);
         Instantiate(hitVFX, transform.position, Quaternion.identity);
+        Instantiate(InGameManager.Instance.InGameData.BottleShardsVFX, transform.position, Quaternion.identity);
     }
 }
