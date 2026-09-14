05fce24a80db8aa5c1bed2e4db8f8fccc7ea10c9
iulian-krafton <craciuniulian25@gmail.com>
2026-09-14 18:22:38 +0300

    Added gamepad support for attack

--- Assets/Scripts/AttacksController.cs
+++ Assets/Scripts/AttacksController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,12 +11,18 @@ public class AttacksController : MonoBehaviour
     Vector2 aimDirection;
     InputAction attackAction;
     InputAction aimDirectionAction;
+
+    InputAction aimDirectionControllerAction;
+    InputAction movementDirectionAction;
+
     PlayerInput PlayerInput => InGameManager.Instance.PlayerInput;
     public bool FacingRight => dotProduct > 0;
 
     private void Start() {
         attackAction = PlayerInput.actions["Attack"];
         aimDirectionAction = PlayerInput.actions["AimDirection"];
+        aimDirectionControllerAction = PlayerInput.actions["AimDirectionController"];
+        movementDirectionAction = PlayerInput.actions["Movement"];
     }
 
     private void Update() {
@@ -26,14 +30,21 @@ public class AttacksController : MonoBehaviour
         if (GlobalData.isPaused) return;
         if (InGameManager.Instance.EventSystem.IsPointerOverGameObject()) return;
 
-        aimDirection = (Camera.main.ScreenToWorldPoint(aimDirectionAction.ReadValue<Vector2>()) - transform.position).normalized;
+        if(Gamepad.current != null)
+        {
+            if (aimDirectionControllerAction.ReadValue<Vector2>().sqrMagnitude > 0.01f)
+                aimDirection = aimDirectionControllerAction.ReadValue<Vector2>().normalized;
+            else if (movementDirectionAction.ReadValue<Vector2>().sqrMagnitude > 0.01f)
+                aimDirection = movementDirectionAction.ReadValue<Vector2>().normalized;
+        }
+        else
+            aimDirection = (Camera.main.ScreenToWorldPoint(aimDirectionAction.ReadValue<Vector2>()) - transform.position).normalized;
+
         aimDirTransform.rotation = Quaternion.LookRotation(forwardRot, aimDirection);
 
         dotProduct = Vector3.Dot(Vector2.right, aimDirection);
 
-        if (attackAction.WasPressedThisFrame()) {
+        if (attackAction.WasPressedThisFrame())
             equippedWeapon.Attack();
-
-        }
     }
 }
