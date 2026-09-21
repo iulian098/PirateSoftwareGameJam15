3db7c20f1cbb05bc4be1d0890f0fb6e0794e4cfa
iulian <craciuniulian25@gmail.com>
2026-09-21 14:30:52 +0300

    Added UIHandCursor script

---
+++ Assets/Scripts/UI/UIHandCursor.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandCursor : MonoSingleton<UIHandCursor>
+{
+    public enum SpriteType
+    {
+        Normal,
+        Click,
+        Drag
+    }
+
+    [SerializeField] Image handImage;
+    [SerializeField] Sprite[] handSprites;
+
+    public void SetHandSprite(SpriteType type)
+    {
+        handImage.sprite = handSprites[(int)type];
+    }
+
+    public void Show()
+    {
+        Cursor.visible = false;
+        handImage.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        Cursor.visible = true;
+        handImage.gameObject.SetActive(false);
+    }
+}
